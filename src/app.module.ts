2d82c370ec07b3ebc2023324ff145c1b8b88ba20
Abdisa Dev <abdisadev@gmail.com>
2024-08-11 20:29:16 +0300

    db setuped'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_NAME,
+      port: +process.env.DB_PORT,
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.PASSWORD,
+      synchronize: true,
+      entities: [User],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
